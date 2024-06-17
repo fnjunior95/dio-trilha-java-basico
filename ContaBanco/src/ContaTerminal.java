53293d96dd9feaa690c3ff92d148d15ce5076b9c
Fabio Junior <fnjunior95@gmail.com>
2024-06-17 18:02:17 -0300

    contaTerminal Funcionando

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.text.DecimalFormat;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String nomeCliente;
+        String agencia;
+        DecimalFormat saldo;
+        System.out.println("Por favor digite o numero da sua conta: ");
+        numero = Integer.parseInt(System.console().readLine());
+        System.out.println("Por favor digite o numero da agencia: ");
+        agencia = System.console().readLine();
+        System.out.println("Por favor digite o nome do cliente: ");
+        nomeCliente = System.console().readLine();
+        System.out.println("Por favor digite o saldo a ser depositado:");
+        
+        saldo = new DecimalFormat (System.console().readLine());
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+        ",conta"+ numero +" e seu saldo " + saldo.format(saldo) + " já está disponivel para saque.");
     }
 }
